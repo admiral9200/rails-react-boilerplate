e0388019f47367b23257216b97accbf5bc81f6ae
admiral9200 <lion9200.world@gmail.com>
2022-02-13 23:37:59 +0300

    Update route, add sample data in seed

--- app/controllers/greetings_controller.rb
+++ app/controllers/greetings_controller.rb
@@ -1,2 +1,6 @@
 class GreetingsController < ApplicationController
+  def index
+    @greetings = Greeting.all.sample
+    render json: @greetings
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
-  get 'static/index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root 'static#index'
+  resources :greetings
 end

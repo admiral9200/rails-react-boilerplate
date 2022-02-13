863ab32f5b312d47831988b4f830ff74ffdf5a89
admiral9200 <lion9200.world@gmail.com>
2022-02-13 23:59:26 +0300

    Update style of greeting page

--- app/javascript/components/Greeting.js
+++ app/javascript/components/Greeting.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { getGreetingData } from '../redux/API';
 import store from '../redux/configureStore';
+import './Greeting.css';
 store.dispatch(getGreetingData);
 
 const Greeting = () => {

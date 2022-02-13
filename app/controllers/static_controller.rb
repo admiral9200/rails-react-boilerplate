cc946f8f9d0a8bc1b2a99fe66226e30b0df335aa
admiral9200 <lion9200.world@gmail.com>
2022-02-14 00:08:53 +0300

    Fix linter errors

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,4 +1,3 @@
 class StaticController < ApplicationController
-  def index
-  end
+  def index; end
 end
--- test/controllers/static_controller_test.rb
+++ test/controllers/static_controller_test.rb
@@ -1,7 +1,7 @@
-require "test_helper"
+require 'test_helper'
 
 class StaticControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
+  test 'should get index' do
     get static_index_url
     assert_response :success
   end
